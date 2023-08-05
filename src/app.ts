55589b074310764cf213ce46ac45baddd8fdc572
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2023-08-05 17:25:46 +0330

    feat: 🎸 complete app
    complete app in src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("Wend-nd")
+import express from "express";
+import helmet from "helmet";
+
+import routes from "./urls";
+import { createProduct } from "./services";
+
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+
+routes(app);
+
+createProduct("Coffee", 28);
+createProduct("Coca Cola", 19);
+
+app.listen(3000, () => {
+    console.log("listening at port 3000");
+});
