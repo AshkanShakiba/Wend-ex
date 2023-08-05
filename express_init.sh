471a9eb2ab110c061cb2acab95cf80bb19f0d146
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2023-08-05 12:34:57 +0330

    feat: 🎸 initial commit
    initialize express application and install dependencies

---
+++ express_init.sh
@@ -0,0 +1,12 @@
+git init
+npx tsc --init
+yarn init
+mkdir src
+touch src/app.ts
+yarn add ts-node-dev typescript -D
+yarn add express
+yarn add @types/node @types/express -D
+yarn add helmet  # optional
+# add this entry to package.json:
+# "scripts": {"dev": "ts-node-dev --respawn --transpile-only src/app.ts"}
+# now you can run the app with `yarn dev`
