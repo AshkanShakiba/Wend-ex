b17931461b8e5c76854234b28ad17ea477b4e2ee
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2023-08-05 16:53:43 +0330

    feat: 🎸 complete views
    implement productDetail, ordersList and orderDetail view in src/views.ts

--- src/views.ts
+++ src/views.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 import { Product, Order } from "./models";
-import { getProduct, getProducts, createOrder } from "./services";
+import { getProduct, getProducts, createOrder, getOrder, getOrders } from "./services";
 
 export function productsList(req: Request, res: Response, next: NextFunction) {
     let output = [];
@@ -13,14 +13,44 @@ export function productsList(req: Request, res: Response, next: NextFunction) {
     return res.json(output);
 }
 
+export function productDetail(req: Request, res: Response, next: NextFunction) {
+    let productId: number = Number(req.params.productId);
+    let product: Product | null = getProduct(productId);
+    if (product == null) {
+        return res.sendStatus(404).json({ error: "product not found" });
+    } else {
+        return res.sendStatus(200).json(product.getObject());
+    }
+}
+
 export function order(req: Request, res: Response, next: NextFunction) {
-    let productId: number = req.body.product;
+    let productId: number = Number(req.params.productId);
     let product: Product | null = getProduct(productId);
     if (product == null) {
         return res.sendStatus(404).json({ error: "product not found" });
     } else {
         createOrder(product);
         return res.sendStatus(200).json({ message: "your order was placed" });
+    }
+}
+
+export function ordersList(req: Request, res: Response, next: NextFunction) {
+    let output = [];
+    let orders: Order[] = getOrders();
+    for (let i = 0; i < orders.length; i++) {
+        let order: Order = orders[i];
+        output.push(order.getObject());
+    }
+    return res.json(output);
+}
+
+export function orderDetail(req: Request, res: Response, next: NextFunction) {
+    let orderId: number = Number(req.params.orderId);
+    let order: Order | null = getOrder(orderId);
+    if (order == null) {
+        return res.sendStatus(404).json({ error: "order not found" });
+    } else {
+        return res.sendStatus(200).json(order.getObject());
     }
 }
 
